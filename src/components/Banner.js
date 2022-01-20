03ed29615d23d953698e9986d3331620bce01ed8
LucasTamayaGIT <lucastamaya.974@gmail.com>
2022-01-20 15:05:15 +0400

    Bouton de telechargement header version mobile et laptop: OK

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,50 +1,52 @@
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
+import discordLogoWhite from "../assets/discord_mini_icon_white.svg";
+import discordLogoBlack from "../assets/discord_mini_icon_black.svg";
+import "../style/Banner.css";
 
 const Banner = () => {
+  const openMenu = () => {
+    document.querySelector(".menu").classList.toggle("disable");
+  };
 
-    const openMenu = () => {
-        document.querySelector(".menu").classList.toggle("disable");
-    };
+  const closeMenu = () => {
+    document.querySelector(".menu").classList.toggle("disable");
+  };
 
-    const closeMenu = () => {
-        document.querySelector(".menu").classList.toggle("disable");
-    };
+  return (
+    <div className="banner">
+      <div className="mobile-banner-left">
+        <img src={discordLogoWhite} alt="discord logo" />
+        <Link to="/">Discord</Link>
+      </div>
 
-    return (
-        <div className="banner" >
-            <div className="mobile-banner-left">
-                <span className="iconify" data-icon="simple-icons:discord"></span>
-                <p>Discord</p>
+      <div className="menu disable">
+        <nav>
+          <div className="nav-top">
+            <div className="discord-logo-nav">
+              <img src={discordLogoBlack} alt="discord logo" />
+              <p>Discord</p>
             </div>
+            <i className="fas fa-times" onClick={closeMenu}></i>
+          </div>
+          <ul className="navigation">
+            <li>Download</li>
+            <li>Nitro</li>
+            <li>Safety</li>
+            <li>Support</li>
+            <li>Blog</li>
+            <li>Careers</li>
+          </ul>
+        </nav>
+      </div>
 
-            <div className="menu disable" >
-                <nav>
-                    <div className="nav-top">
-                        <div className="discord-logo-nav">
-                            <span className="iconify" data-icon="simple-icons:discord"></span>
-                            <p>Discord</p>
-                        </div>
-                        <i className="fas fa-times" onClick={closeMenu}></i>
-                    </div>
-                    <ul className="navigation">
-                        <li>Download</li>
-                        <li>Nitro</li>
-                        <li>Safety</li>
-                        <li>Support</li>
-                        <li>Blog</li>
-                        <li>Careers</li>
-                    </ul>
-                </nav>
-            </div>
-
-            <div className="mobile-banner-right">
-                <Link class="to-login-page" to="/login">Login</Link>
-                <i className="fas fa-bars" id="open-menu" onClick={openMenu}></i>
-            </div>
-        </div>
-
-    )
-
-}
+      <div className="mobile-banner-right">
+        <Link class="to-login-page" to="/login">
+          Login
+        </Link>
+        <i className="fas fa-bars" id="open-menu" onClick={openMenu}></i>
+      </div>
+    </div>
+  );
+};
 
 export default Banner;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,33 @@
-import "../style/Header.css"
-import Banner from "./Banner"
+import "../style/Header.css";
+import Banner from "./Banner";
 // import image from "../assets/"
 
 const Header = () => {
+  return (
+    <header>
+      <Banner />
+      <div className="header-text">
+        <h1> IMAGINE A PLACE... </h1>
+        <p>
+          ...where you can belong to a school club, a gaming group, or a
+          worldwide art community. Where just you and a handful of friends can
+          spend time together. A place that makes it east to talk every day and
+          hang out more often.
+        </p>
+      </div>
 
-    return (
+      <div className="discord-btn-container">
+        <button className="discord-btn">
+          <img src="https://img.icons8.com/pastel-glyph/64/000000/download--v1.png" />
+          Download for Mac
+        </button>
 
-        <header>
-            <Banner />
-            
-            <h1>IMAGINE A PLACE...</h1>
-
-            <p>...where you can belong to a school club, a gaming group, or a worldwide art community.
-                Where just you and a handful of friends can spend time together. A place that makes it
-                east to talk every day and hang out more often.
-            </p>
-
-            <div className="image">
-
-            </div>
-
-        </header>
-
-
-
-    )
-
-}
+        <button className="discord-btn open-browser">
+          Open Discord in your browser
+        </button>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
