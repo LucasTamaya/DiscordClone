c772152abbdd988232c1030dfebed335b39a99bc
LucasTamayaGIT <lucastamaya.974@gmail.com>
2022-01-20 08:16:02 +0400

    Login version laptop: OK; manque juste le titre et le logo discord en haut a gauche

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,32 +5,42 @@ const Login = () => {
 
     return (
 
-        <main>
+        <main className="login-bg">
             <div className="login-container">
-                <Link to="/">
-                    <div className="discord-logo">
-                        <span className="iconify" data-icon="simple-icons:discord"></span>
-                        <p>Discord</p>
-                    </div>
+                <Link to="/" className="discord-logo">
+                    <span className="iconify" data-icon="simple-icons:discord"></span>
+                    <p>Discord</p>
                 </Link>
 
-                <div className="login-text">
-                    <h2>Welcome back!</h2>
-                    <p>We're exited to see you again!</p>
+                <div className="login-laptop-left">
+                    <div className="login-text">
+                        <h3>Welcome back!</h3>
+                        <p>We're exited to see you again!</p>
+                    </div>
+
+                    <form>
+                        <div>
+                            <label>EMAIL</label>
+                            <input type="text" />
+                        </div>
+                        <div>
+                            <label>PASSWORD</label>
+                            <input type="text" />
+                        </div>
+
+                        <p>Forgot your password?</p>
+
+                        <button type="submit" className="login-btn">Login</button>
+                        <p><span>Need an account?</span> Register</p>
+                    </form>
                 </div>
 
-                <form>
-                    <label>EMAIL</label>
-                    <input type="text" />
+                <div className="login-laptop-right">
+                    <div className="qr-code"></div>
+                    <h2>Log in with QR Code</h2>
+                    <p>Scan this with the <span>Discord mobile app</span> to log in instantly.</p>
+                </div>
 
-                    <label>PASSWORD</label>
-                    <input type="text" />
-
-                    <p>Forgot your password?</p>
-
-                    <button type="submit" class="login-btn">Login</button>
-                    <p><span>Need an account?</span> Register</p>
-                </form>
 
             </div>
         </main>
